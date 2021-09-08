370dd7e262e4515fa0eef33f6d1f063377cbc17f
Raihan Stark <raihanyudosaputra30@gmail.com>
2021-09-08 11:15:27 +0700

    update bubble sort testing

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,17 +1,15 @@
-const arrayNum = [1,5,5,2,6,8,4,20,34,50,70,75,30,11]
-
-function bubbleSort(arraySort){
-  for (let i = 0; i < arraySort.length; i++){
-   for (let j = 0; j < arraySort.length; j++){
-    if (arraySort[j] > arraySort[j + 1]){
-      let temp = arraySort[j]
-      arraySort[j] = arraySort[j + 1];
-      arraySort[j + 1] = temp
+function bubbleSort(arraySort) {
+  for (let i = 0; i < arraySort.length; i++) {
+    for (let j = 0; j < arraySort.length; j++) {
+      if (arraySort[j] > arraySort[j + 1]) {
+        let temp = arraySort[j];
+        arraySort[j] = arraySort[j + 1];
+        arraySort[j + 1] = temp;
+      }
     }
-   }
   }
-  
+
   return arraySort;
 }
 
-console.log(bubbleSort(arrayNum))
+module.exports = bubbleSort;
---
+++ bubble-sort.test.js
@@ -0,0 +1,19 @@
+const bubbleSort = require("./bubble-sort");
+
+describe("Bubble Sort", () => {
+  let dataList;
+
+  beforeEach(() => {
+    dataList = [1, 5, 5, 2, 6, 8, 4, 20, 34, 50, 70, 75, 30, 11];
+  });
+
+  it("should be match", () => {
+    expect(bubbleSort(dataList)).toEqual([
+      1, 2, 4, 5, 5, 6, 8, 11, 20, 30, 34, 50, 70, 75,
+    ]);
+  });
+
+  it("is array is empty", () => {
+    expect(bubbleSort([])).toEqual([]);
+  });
+});
