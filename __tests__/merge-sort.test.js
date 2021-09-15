a0839666d3109148bd5e116bd685c74b0cd9fc92
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2021-09-15 12:19:50 +0700

    add merge sort tests

---
+++ __tests__/merge-sort.test.js
@@ -0,0 +1,19 @@
+const mergeSort = require("../src/merge-sort");
+
+describe("Merge Sort", () => {
+  let dataList;
+
+  beforeEach(() => {
+    dataList = [1, 5, 5, 2, 6, 8, 4, 20, 34, 50, 70, 75, 30, 11];
+  });
+
+  it("should be match", () => {
+    expect(mergeSort(dataList)).toEqual([
+      1, 2, 4, 5, 5, 6, 8, 11, 20, 30, 34, 50, 70, 75,
+    ]);
+  });
+
+  it("is array is empty", () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+});
